c59ec702f68ec5ebf67887dd0011c4c4e069a468
barak1412 <barak1413@gmail.com>
2020-02-21 12:29:43 +0200

    Added python class wrapper.

--- Python/my_physics.pyx
+++ Python/my_physics.pyx
@@ -7,3 +7,15 @@ def velocity_api(v0, a, t):
 	cdef float result[1]
 	cmy_physics.velocity_api(v0, a, t, result)
 	return result[0]
+
+class PhysicsWrapper(object):
+	
+	@staticmethod
+	def gforce(weight):
+		return cmy_physics.gforce_api(weight)
+	
+	@staticmethod
+	def velocity(v0, a, t):
+		cdef float result[1]
+		cmy_physics.velocity_api(v0, a, t, result)
+		return result[0]
