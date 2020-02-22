133cd85ea8d7326898623cc332a1813e195ef949
barak1412 <barak1413@gmail.com>
2020-02-22 19:12:55 +0200

    Changed folder name.

--- Python/setup.py
+++ Python/setup.py
@@ -3,7 +3,7 @@ import os
 
 def generate_sources_lst():
 	sources = ["my_physics.bycython.cpp"]
-	sources_dirs = ["MyToolBox/Source/", "MathematicsOps/Source/"]
+	sources_dirs = ["PhysicsOps/Source/", "MathematicsOps/Source/"]
 	for source_dir in sources_dirs:
 		sources += [os.path.join(source_dir, f) for f in os.listdir(source_dir) if f.endswith(".cpp") or f.endswith(".c")]
 	return sources
@@ -18,6 +18,6 @@ setup(
     # Build instructions
     ext_modules = [Extension("my_physics",
                              sources=generate_sources_lst(),
-							 include_dirs=["MathematicsOps/Includes", "MyToolBox/Includes"],
+							 include_dirs=["MathematicsOps/Includes", "PhysicsOps/Includes"],
 							 language="c++")]
 )
